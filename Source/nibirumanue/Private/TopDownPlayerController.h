f0fa33d212e95d2d92d2d9cc62b1dbd4595e1a12
tasogare66 <hijiki_umai@hotmail.co.jp>
2023-04-23 15:18:44 +0900

    TopDownPlayerを追加

---
+++ Source/nibirumanue/Private/TopDownPlayerController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "TopDownPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ATopDownPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
